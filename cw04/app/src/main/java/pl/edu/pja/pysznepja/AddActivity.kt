51e604c6f4e90c9e621b1b7718eba81e0c19d1c1
Mateusz <s19282@pjwstk.edu.pl>
2021-04-28 20:26:15 +0200

    added dishes images, ImageAdapter.kt, etc.

--- cw04/app/src/main/java/pl/edu/pja/pysznepja/AddActivity.kt
+++ cw04/app/src/main/java/pl/edu/pja/pysznepja/AddActivity.kt
@@ -1,11 +1,40 @@
 package pl.edu.pja.pysznepja
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import pl.edu.pja.pysznepja.adapter.ImageAdapter
+import pl.edu.pja.pysznepja.databinding.ActivityAddBinding
+import pl.edu.pja.pysznepja.databinding.ActivityMainBinding
 
 class AddActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityAddBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        setContentView(binding.root)
+        setupRecycler()
+    }
+
+    private fun loadDrawables(): List<Drawable>
+    {
+        val drawableIds = listOf(
+            R.drawable.pierogi,
+            R.drawable.pizza,
+            R.drawable.pumpkin,
+            R.drawable.spaghetti,
+            R.drawable.rosol,
+            R.drawable.rice
+        )
+        return drawableIds.map { resources.getDrawable(it,theme) }
+    }
+
+    private fun setupRecycler() {
+        binding.imagesList.apply {
+            adapter = ImageAdapter(loadDrawables())
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        }
     }
 }
---
+++ cw04/app/src/main/java/pl/edu/pja/pysznepja/adapter/ImageAdapter.kt
@@ -0,0 +1,53 @@
+package pl.edu.pja.pysznepja.adapter
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import pl.edu.pja.pysznepja.databinding.ItemImageBinding
+
+class DishImage(private val binding: ItemImageBinding) : RecyclerView.ViewHolder(binding.root)
+{
+    fun bindImage(drawable: Drawable)
+    {
+        binding.image.setImageDrawable(drawable)
+    }
+    fun setSelection(state: Boolean)
+    {
+        binding.frame.visibility = if (state) View.VISIBLE else View.INVISIBLE
+    }
+}
+
+class ImageAdapter(private val images: List<Drawable>): RecyclerView.Adapter<DishImage>()
+{
+    var selectedItem: Int? = null
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DishImage {
+        val binding = ItemImageBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return DishImage(binding).also { holder -> binding.root.setOnClickListener{
+            setSelection(holder.layoutPosition)
+        } }
+    }
+
+    private fun setSelection(layoutPosition: Int) {
+        selectedItem?.let {
+            selectedItem = layoutPosition
+            notifyItemChanged(it)
+        }
+        notifyItemChanged(layoutPosition)
+    }
+
+    override fun onBindViewHolder(holder: DishImage, position: Int) {
+        holder.apply {
+            bindImage(images[position])
+            setSelection(position == selectedItem)
+        }
+    }
+
+    override fun getItemCount(): Int = images.size
+
+}
