806ac8bd42fd58020eaabf84ac1fd076a358b311
Mateusz <s19282@pjwstk.edu.pl>
2021-05-03 20:16:19 +0200

    new app icon

--- FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/db/Shared.kt
+++ FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/db/Shared.kt
@@ -8,14 +8,14 @@ object Shared
     val transactionList = mutableListOf(
             Transaction(2500.0,"Store", LocalDate.now().minusDays(30),0,0),
             Transaction(500.0,"Cinema", LocalDate.now().minusDays(30),1,1),
-            Transaction(600.0,"Car repair shop", LocalDate.now().minusDays(29),2,0),
-            Transaction(800.0,"Taxes", LocalDate.now().minusDays(29),3,1),
-            Transaction(600.0,"Food", LocalDate.now().minusDays(27),4,1),
-            Transaction(400.0,"Food", LocalDate.now().minusDays(26),4,0),
-            Transaction(400.0,"Food", LocalDate.now().minusDays(25),4,1),
-            Transaction(1100.0,"Food", LocalDate.now().minusDays(24),4,1),
-            Transaction(100.0,"Food", LocalDate.now().minusDays(23),4,0),
-            Transaction(150.0,"Food", LocalDate.now().minusDays(22),4,1),
-            Transaction(450.0,"Food", LocalDate.now().minusDays(21),4,1),
+//            Transaction(600.0,"Car repair shop", LocalDate.now().minusDays(29),2,0),
+//            Transaction(800.0,"Taxes", LocalDate.now().minusDays(29),3,1),
+//            Transaction(600.0,"Food", LocalDate.now().minusDays(27),4,1),
+//            Transaction(400.0,"Food", LocalDate.now().minusDays(26),4,0),
+//            Transaction(400.0,"Food", LocalDate.now().minusDays(25),4,1),
+//            Transaction(1100.0,"Food", LocalDate.now().minusDays(24),4,1),
+//            Transaction(100.0,"Food", LocalDate.now().minusDays(23),4,0),
+//            Transaction(150.0,"Food", LocalDate.now().minusDays(22),4,1),
+//            Transaction(450.0,"Food", LocalDate.now().minusDays(21),4,1),
     )
 }
