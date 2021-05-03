89722334efaf7eb93befcc0c34914a027975b59d
Mateusz <s19282@pjwstk.edu.pl>
2021-05-03 12:20:00 +0200

    chart work a bit

--- FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/MonthlyChart.kt
+++ FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/MonthlyChart.kt
@@ -7,11 +7,18 @@ import android.graphics.Paint
 import android.icu.util.Calendar
 import android.util.AttributeSet
 import android.view.View
+import java.time.LocalDate
+import kotlin.math.absoluteValue
 
 class MonthlyChart(context: Context, attrs: AttributeSet) : View(context, attrs)
 {
     private val paint = Paint().apply {
         color = Color.BLACK
+        strokeWidth = 10f
+        textSize = 60f
+    }
+    private val paint2 = Paint().apply {
+        color = Color.RED
         strokeWidth = 10f
         textSize = 60f
     }
@@ -22,13 +29,64 @@ class MonthlyChart(context: Context, attrs: AttributeSet) : View(context, attrs)
         canvas ?: return
         with(canvas)
         {
-            drawLine(0f, height.toFloat() * .5f,width.toFloat()+100f, height.toFloat() * .5f, paint)
+            val all = Shared.transactionList.groupBy { it.date }.values.map {
+                var tmp = 0.0
+                it.forEach{ t->
+                    tmp +=  if(t.type==0) t.amount
+                    else t.amount*-1
+                }
+                tmp
+            }
+
+            val minValue: Double = all.minOf { it }
+            val maxValue: Double = all.maxOf { it }
+            val heightOf0 = if(maxValue>0 && minValue <0) maxValue/(maxValue+minValue.absoluteValue)
+                            else if(maxValue<=0) 0.0
+                            else 1.0
+//                            else if(minValue>=0) 1.0
+//                            else -1.0
+            val scaledHeightOf0 = height.toFloat() * .1f + height.toFloat() * .8f * heightOf0.toFloat()
+
+            drawLine(0f, scaledHeightOf0,width.toFloat()+100f, scaledHeightOf0, paint)
             val daysInMonth = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH)
             var xTmp = width.toFloat() / daysInMonth
             for (i in 1..daysInMonth)
             {
-                drawText(i.toString().plus("\t"), xTmp, height.toFloat() * .55f, paint)
+                drawText(i.toString().plus("\t"), xTmp, scaledHeightOf0 *1.05f, paint)
                 xTmp += width.toFloat() / daysInMonth
+            }
+
+            val daysToShow = 30
+            val dailyTransactions = Shared.transactionList.filter { it.date.isAfter(LocalDate.now().minusDays(daysToShow.toLong())) }.groupBy { it.date }
+
+//                    .values.map {
+//                var tmp = 0.0
+//                it.forEach{ t->
+//                    tmp +=  if(t.type==0) t.amount
+//                    else t.amount*-1
+//                }
+//                tmp
+//            }
+
+
+
+            var previousY = scaledHeightOf0
+            var previousX = 0f
+            xTmp = width.toFloat() / daysInMonth
+            for (i in 0..daysToShow)
+            {
+                val actualY = dailyTransactions[LocalDate.now().minusDays(i.toLong())]?.sumBy {
+                    var tmp = 0.0
+                    tmp +=  if(it.type==0) it.amount
+                    else it.amount*-1
+                    tmp.toInt()
+                }?.toFloat() ?: previousY
+                val scaledActualY =(maxValue-actualY/ (maxValue-minValue)).toFloat()
+                val actualX = previousX + xTmp
+                drawLine(previousX,previousY,actualX,scaledActualY,paint2)
+
+                previousX = actualX
+                previousY = scaledActualY
             }
         }
     }
--- FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/Shared.kt
+++ FinancialManager/app/src/main/java/pl/edu/pja/financialmanager/Shared.kt
@@ -8,8 +8,8 @@ object Shared
     val transactionList = mutableListOf(
             Transaction(400.0,"Store", LocalDate.now(),0,0),
             Transaction(500.0,"Cinema", LocalDate.now(),1,1),
-            Transaction(600.0,"Car repair shop", LocalDate.now().plusDays(1),2,0),
-            Transaction(700.0,"Taxes", LocalDate.now().plusDays(1),3,1),
-            Transaction(800.0,"Food", LocalDate.now().plusDays(2),4,0)
+            Transaction(600.0,"Car repair shop", LocalDate.now().minusDays(1),2,0),
+            Transaction(700.0,"Taxes", LocalDate.now().minusDays(1),3,1),
+            Transaction(800.0,"Food", LocalDate.now().minusDays(2),4,0)
     )
 }
