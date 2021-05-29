34ddd5ef2ec702468882dae41dd45b85bfc18c69
Mateusz <s19282@pjwstk.edu.pl>
2021-05-29 22:04:16 +0200

    picture with date on it

--- TravelerApp/app/src/main/java/pl/edu/pja/travelerapp/MainActivity.kt
+++ TravelerApp/app/src/main/java/pl/edu/pja/travelerapp/MainActivity.kt
@@ -1,39 +1,59 @@
 package pl.edu.pja.travelerapp
 
+import android.content.ContentValues
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.*
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import androidx.core.content.FileProvider
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import pl.edu.pja.travelerapp.databinding.ActivityMainBinding
+import java.io.File
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.*
 
+
+const val CAMERA_PERMISSIONS_REQUEST = 1
+const val CAMERA_INTENT_REQUEST = 2
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var uri = Uri.EMPTY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.openCamera.setOnClickListener{
-            val uri = generateURI()
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).let {
-                it.putExtra(MediaStore.EXTRA_OUTPUT,uri)
-            }
-            startActivityForResult(intent, 1)
+            generateURI()
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT,uri)
+
+            startActivityForResult(intent, CAMERA_INTENT_REQUEST)
         }
     }
 
-    private fun generateURI(): Uri {
-        val file = filesDir.resolve("img.jpg").also {
-            it.writeText("")
+
+    private fun generateURI() {
+        val tmp = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         }
-        return FileProvider.getUriForFile(
-            this,
-            "pl.edu.pja.travelerApp.FileProvider",
-            file
-        )
+        val contentValues = ContentValues().apply {
+            put(
+                MediaStore.Images.Media.DISPLAY_NAME, SimpleDateFormat(
+                "yyyy-MM-dd-HH-mm-ss-SSS"
+                , Locale.US)
+                    .format(System.currentTimeMillis()) + ".jpg"
+            )
+            put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        }
+        uri = contentResolver?.insert(tmp, contentValues)
+
     }
 
 //    override fun onRequestPermissionsResult(
@@ -52,14 +72,35 @@ class MainActivity : AppCompatActivity() {
 //        }
 //    }
 
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == 1 && resultCode == RESULT_OK)
+        if(requestCode == CAMERA_INTENT_REQUEST && resultCode == RESULT_OK)
         {
+            val bitmap = ImageDecoder.decodeBitmap(ImageDecoder.createSource(this.contentResolver, uri)).copy(Bitmap.Config.ARGB_8888,true)
+
             binding.imageView2.setImageBitmap(
-                BitmapFactory.decodeFile(filesDir.resolve("img.jpg").absolutePath)
+                bitmap.drawText()
             )
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun Bitmap.drawText(
+        text:String = LocalDate.now().toString(),
+        textSize:Float = 55f,
+        color:Int = Color.RED
+    ):Bitmap?{
+        val bitmap = copy(config,true)
+        val canvas = Canvas(bitmap)
+
+        Paint().apply {
+            flags = Paint.ANTI_ALIAS_FLAG
+            this.color = color
+            this.textSize = textSize
+            typeface = Typeface.SERIF
+            setShadowLayer(1f,0f,1f,Color.WHITE)
+            canvas.drawText(text,20f,height - 20f,this)
+        }
+
+        return bitmap
     }
 }
