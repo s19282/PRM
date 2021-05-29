9d95d4e96d3d66d142e341817a943890f560333c
Mateusz <s19282@pjwstk.edu.pl>
2021-05-29 13:40:30 +0200

    added camera using CameraX

--- Traveler/app/src/main/java/pl/edu/pja/traveler/MainActivity.kt
+++ Traveler/app/src/main/java/pl/edu/pja/traveler/MainActivity.kt
@@ -1,11 +1,30 @@
 package pl.edu.pja.traveler
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import pl.edu.pja.traveler.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+private const val REQUEST_CAMERA_TRANSFER = 1
+
+class MainActivity : AppCompatActivity()
+{
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.title = "Your photos"
+        setContentView(binding.root)
+        binding.cameraButton.setOnClickListener(openCameraActivity())
+    }
+
+    private fun openCameraActivity() : View.OnClickListener
+    {
+        return View.OnClickListener {
+            startActivityForResult(
+                Intent(this, CameraActivity::class.java),
+                REQUEST_CAMERA_TRANSFER
+            ) }
     }
 }
