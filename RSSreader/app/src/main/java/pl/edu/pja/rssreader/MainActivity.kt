1d244708325191b39e7850da3b56b414ce2194cc
Mateusz <s19282@pjwstk.edu.pl>
2021-06-19 10:14:34 +0200

    firebase test

--- RSSreader/app/src/main/java/pl/edu/pja/rssreader/MainActivity.kt
+++ RSSreader/app/src/main/java/pl/edu/pja/rssreader/MainActivity.kt
@@ -2,10 +2,37 @@ package pl.edu.pja.rssreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun crashApp(view: View)
+    {
+        if(auth.currentUser == null)
+        {
+            auth.createUserWithEmailAndPassword("asdfasdf@gmail.com","234567")
+                .addOnSuccessListener { Toast.makeText(this,"Zarejestrowano",Toast.LENGTH_LONG).show() }
+            auth.signInWithEmailAndPassword("asdfasdf@gmail.com", "234567")
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Zalogowano ${it.user?.uid} ${it.user?.email}",Toast.LENGTH_LONG).show()
+                }
+        }
+        else
+        {
+            Toast.makeText(this, "Zalogowano ${auth.currentUser?.uid} ${auth.currentUser?.email}",Toast.LENGTH_LONG).show()
+            FirebaseDatabase.getInstance()
+                .getReference("users")
+                .child("${auth.currentUser?.uid}")
+                .setValue("${auth.currentUser?.email}")
+        }
     }
 }
